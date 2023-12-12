57f49f96c73083d34a8e3ab8d428838ddfdb3cf4
wandersonsantos01 <~wanderson.santos01@gmail.com>
2023-12-11 21:57:25 -0300

    Implementing jetty-servlet service

---
+++ ecommerce/service-http-ecommerce/src/main/java/org/example/HttpEcommerceService.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class HttpEcommerceService {
+
+    public static void main(String[] args) throws Exception {
+
+        Server server = new Server(8080);
+
+        ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath("/");
+        context.addServlet(new ServletHolder(new NewOrderServlet()), "/new");
+
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
+
+}
